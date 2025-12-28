ce51464fe598bbc14ee2b66d84df37a2511228ed
zacksloth5 <zackgourevitch@gmail.com>
2025-12-28 17:46:31 -0500

    More Paths
    Added variables for eating and button states in Path.

--- state.py
+++ state.py
@@ -16,9 +16,16 @@ class Path:
         return f"Path({self._data})"
 st = Path()
 
+#Eating Path Variables
 st.eat.eatClock = 1200
 st.eat.ranout = ""
 st.eat.food = True
 st.eat.starving = False
 st.eat.lastEatTime = time.time()
 st.eat.eatInterval = 20
+
+#Button Path Variables
+st.gui.buttons.branstorm.onScreen = True
+st.gui.buttons.berries.onScreen = True
+st.gui.buttons.research.onScreen = False
+
