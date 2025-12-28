60774a93a071781612865714b63dc4280afeb989
zacksloth5 <zackgourevitch@gmail.com>
2025-12-28 17:43:08 -0500

    Added States which are essential for pathing and can hold many variables

--- state.py
+++ state.py
@@ -1,8 +1,24 @@
 import time
-eatclock = 1200
-ranout = ""
-food = True
-starving = False
-last_eat_time = time.time()
-eat_interval = "20"
-printedmessage = False
+
+class Path:
+    def __init__(self):
+        self.__dict__["_data"] = {}
+
+    def __getattr__(self, name):
+        if name not in self._data:
+            self._data[name] = Path()
+        return self._data[name]
+
+    def __setattr__(self, name, value):
+        self._data[name] = value
+
+    def __repr__(self):
+        return f"Path({self._data})"
+st = Path()
+
+st.eat.eatClock = 1200
+st.eat.ranout = ""
+st.eat.food = True
+st.eat.starving = False
+st.eat.lastEatTime = time.time()
+st.eat.eatInterval = 20
